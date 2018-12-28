beb51c03eb442c3d4cb64ef6a142e476219f2b43
Alireza-Khezri <ali.khezri1986@gmail.com>
2018-12-29 00:58:04 +0330

    display login view with close button to return to called view

---
+++ Smack/Smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Macbook on 12/29/18.
+//  Copyright © 2018 Macbook. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
---
+++ Smack/Smack/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Macbook on 12/29/18.
+//  Copyright © 2018 Macbook. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogin"
